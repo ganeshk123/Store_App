108ab603f8c3e57fcdb62debbb353ca419b946ef
“ganeshk123” <ganesh.korde7@gmail.com>
2021-03-27 22:48:53 +0530

    Store_App

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "onesignal-cordova-plugin.OneSignal",
+      "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+      "pluginId": "onesignal-cordova-plugin",
+      "clobbers": [
+        "OneSignal"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.IonicWebView",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "Ionic.WebView"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "cordova.exec"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "onesignal-cordova-plugin": "2.11.1",
+    "cordova-plugin-ionic-webview": "4.2.1"
+  };
+});
